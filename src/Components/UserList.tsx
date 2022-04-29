0371513754e56ffeb281e617bdba021185b78f34
deviluppercase <deviluppercase@gmail.com>
2022-04-29 21:34:14 +0100

    Cant use a .map of users in UserList.tsx

--- src/Components/UserList.tsx
+++ src/Components/UserList.tsx
@@ -3,15 +3,18 @@ import { UserContext } from "../Context/UserContext";
 
 export const UsersList = (props: any): JSX.Element => {
 
- // const [, setSecond] = useState(0);
-
+ 
+ /** @Error if --> const {users} = useContext(UserContext);
+ Se declara "users", pero su valor no se lee nunca.ts(6133)
+ La propiedad 'users' no existe en el tipo 'IUserContext | IUser[]'.*/
   const value = useContext(UserContext);
   
 
   //<MyContext.Provider value={/* some value */}></MyContext.Provider>
   return (
     <>
-      
+      {`quiero hacer un map con li de users que 
+      se actualice cada vez que cambia el context`}
     </>
   );
 }
